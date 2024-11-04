65a0c67686d0cedb2dfd1b69c683f847e1b549f2
vboxuser <vboxuser@ubuntu2.myguest.virtualbox.org>
2024-11-04 14:45:16 +0300

    rayan last change deleted prints

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,17 +1,17 @@
+import scapy
 from scapy.all import sniff, IP, ICMP
 
 def handle_packet(packet):
     if IP in packet and ICMP in packet:
-        # Check if it is an ICMP request (Type 8 is Echo Request)
+        
         if packet[ICMP].type == 8:
-            print("Received ICMP request packet:")
             packet.show()
 
 def receive_icmp():
-    print("Listening for ICMP packets...")
-    # Capture ICMP packets
-    sniff(filter="icmp", prn=handle_packet,count=1)
-    return
+    
+    sniff(filter="icmp", prn=handle_packet, count=1)
+
 if __name__ == "__main__":
     receive_icmp()
-# Implement your ICMP receiver here
+
+
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,23 +1,17 @@
+import scapy
 from scapy.all import ICMP, IP, send
-
 def send_icmp():
-    # Define the destination IP (replace with the receiver container's IP)
-    destination_ip = "receiver"
 
-    # Create an IP packet with TTL = 1
+    destination_ip = "172.18.0.2"
+
     ip_packet = IP(dst=destination_ip, ttl=1)
 
-    # Create an ICMP packet
     icmp_packet = ICMP()
 
-    # Combine the IP and ICMP packet
     packet = ip_packet / icmp_packet
 
-    # Send the packet
-    print("Sending ICMP packet...")
     send(packet)
-    print("ICMP packet sent.")
 
 if __name__ == "__main__":
     send_icmp()
-# Implement your ICMP sender here
+
